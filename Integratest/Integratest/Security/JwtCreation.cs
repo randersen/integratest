0267901fd5195474244112ba776b874f071d1277
randersen <ryan.andersen91@gmail.com>
2017-01-02 23:48:58 -0700

    Added JWT support (maybe)

---
+++ Integratest/Integratest/Security/JwtCreation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integratest.Security
+{
+    public class JwtHelpers
+    {
+        private static readonly string _jwtSecret = Environment.GetEnvironmentVariable("INTEGRATEST_JWT_SECRET");
+
+        public static string CreateJwt(JwtRequest request)
+        {
+            var payload = new Dictionary<string, object>()
+            {
+                { "email", request.UserEmail },
+                { "id", request.UserId }
+            };
+            var secretKey = _jwtSecret;
+            string token = JWT.JsonWebToken.Encode(payload, secretKey, JWT.JwtHashAlgorithm.HS256);
+            return token;
+        }
+
+        public static IDictionary<string, object> DecodeJwt(string token)
+        {
+            try
+            {
+                var payload = JWT.JsonWebToken.DecodeToObject(token, _jwtSecret) as IDictionary<string, object>;
+                return payload;
+            }
+            catch (JWT.SignatureVerificationException)
+            {
+                Console.WriteLine("Invalid token!");
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ Integratest/Integratest/Security/JwtRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integratest.Security
+{
+    public class JwtRequest
+    {
+        public string UserEmail { get; set; }
+        public string UserId { get; set; }
+    }
+}
